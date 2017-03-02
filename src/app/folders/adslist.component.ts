dddcec546a11f727bb0f40b10c666a64de5beb44
Tomaz Toplak <tomaz@arabsu.com>
2017-03-03 00:45:07 +0100

    Display the current folder path

--- src/app/folders/adslist.component.ts
+++ src/app/folders/adslist.component.ts
@@ -16,30 +16,56 @@ import { FolderService } from './folder.service';
 
 export class AdsListComponent implements OnInit {
 	title = 'Ad API client';
+	currentLocation = "";
 	folders: Folder[];
-	currentFolder: Folder;
+	currentPath: Folder[];
   
 	constructor(
 		private folderService: FolderService,
 		private route: ActivatedRoute,
 		private router: Router
-	) {}
+	) {
+		this.currentPath = [];		
+	}
   
 	getFolders(fId): void {
 		this.folderService.getFolders(fId).then(folders => this.folders = folders);
 	}
-  
+	
+	//compose the path string from the path array
+	setPathString(): void {
+		this.currentLocation = "";
+		for (var i = 0; i < this.currentPath.length; i++){
+			this.currentLocation += this.currentPath[i].name + "/";
+		}
+	}
+	
 	ngOnInit(): void {
 		this.route.params
 		// (+) converts string 'id' to a number
 		.switchMap((params: Params) => {
-			var id = +params['id'];
-			return this.folderService.getFolders(id);
+			return this.folderService.getFolders(+params['id']);
 		})
-		.subscribe((folders: Folder[]) => this.folders = folders);			
-	}  
-	
+		.subscribe((folders: Folder[]) => {
+			this.folders = folders;
+			this.setPathString();
+		});			
+	} 
+
 	onSelect(folder: Folder): void {
+		//push the new folder to the path
+		this.currentPath.push(folder);		
 		this.router.navigate(['/folder', folder.id]);
+	}
+	
+	goToParrent(): void {
+		var parentid = 0;
+		if(this.currentPath.length > 0){
+			parentid = this.currentPath[this.currentPath.length-1].parentid;			
+		}		
+		//remove the folder from path
+		this.currentPath.splice(-1,1);
+		
+		this.router.navigate(['/folder', parentid]);
 	}
 }
